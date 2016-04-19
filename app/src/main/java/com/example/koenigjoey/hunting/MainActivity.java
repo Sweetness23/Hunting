b9fe728ee74335ec486ca32ba5501b2709a724b2
Sweetness23 <koenigjoey@gmail.com>
2016-04-19 18:59:20 -0500

    0.0.2

--- app/src/main/java/com/example/koenigjoey/hunting/MainActivity.java
+++ app/src/main/java/com/example/koenigjoey/hunting/MainActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.BackendlessCallback;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String appVersion = "v1";
+        Backendless.initApp(this, "B95D5D4D-ED7C-6ECD-FF03-48CCECB16900", "2D395DBF-E383-C5BF-FFE9-1916F1D78C00", appVersion);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         view = (ImageView)findViewById(R.id.photo);
@@ -30,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(photoCaptureIntent, requestCode);
+            }
+        });
+
+        Button save = (Button)findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                BackendlessUser user = new BackendlessUser();
+                user.setEmail( "wochol35@gmail.com" );
+                user.setPassword( "piper" );
+
+                Backendless.UserService.register( user, new BackendlessCallback<BackendlessUser>()
+                {
+                    @Override
+                    public void handleResponse( BackendlessUser backendlessUser )
+                    {
+                        Log.i("Registration", backendlessUser.getEmail() + " successfully registered");
+                    }
+                } );
             }
         });
 
